e9c2c3b43218d333b45526248c5fe49ce89579b4
xiongxiong <xiongxiong@staff.sina.com.cn>
2018-10-20 15:58:38 +0800

    update change2

--- shiyanlou/zhou_1/change2.py
+++ shiyanlou/zhou_1/change2.py
@@ -5,28 +5,32 @@ import sys
 yginf={}
 def getTrueGongzi(gongzi):
     wuxianm=(0.08+0.02+0.005+0.06)*gongzi
-    yingna=gongzi-wuxianm-3500
-    if yingna<=1500:
-        shuim=yingna*0.03-0
-    elif yingna>1500 and yingna<=4500:
-        shuim =yingna*0.1-105
-
-    elif yingna>4500 and yingna<=9000:
-        shuim =yingna*0.2-555
-    elif yingna>9000 and yingna<=35000:
-        shuim =yingna*0.25-1005
-    elif yingna>35000 and yingna<=55000:
-        shuim =yingna*0.3-2755
-    elif yingna>55000 and yingna<=80000:
-        shuim =yingna*0.35-5505
+    yingnaPre=gongzi-wuxianm
+    if yingnaPre<=3500:
+        result = gongzi - wuxianm
     else:
-        shuim = yingna * 0.45 - 13505
-    result=gongzi-wuxianm-shuim
+        yingna=gongzi-wuxianm-3500
+        if yingna<=1500:
+            shuim=yingna*0.03-0
+        elif yingna>1500 and yingna<=4500:
+            shuim =yingna*0.1-105
+
+        elif yingna>4500 and yingna<=9000:
+            shuim =yingna*0.2-555
+        elif yingna>9000 and yingna<=35000:
+            shuim =yingna*0.25-1005
+        elif yingna>35000 and yingna<=55000:
+            shuim =yingna*0.3-2755
+        elif yingna>55000 and yingna<=80000:
+            shuim =yingna*0.35-5505
+        else:
+            shuim = yingna * 0.45 - 13505
+        result=gongzi-wuxianm-shuim
     return result
 
 def printResult():
     for key,val in yginf.items():
-        print("{key:val}".format(key=key,val=format(val,".2f")))
+        print("{key}:{val}".format(key=key,val=format(val,".2f")))
 
 def getGongzi(gonghao,gongzi):
     result=getTrueGongzi(gongzi)
@@ -35,6 +39,8 @@ def getGongzi(gonghao,gongzi):
 
 
 def getarg():
+    #print("getarg")
+    #print(sys.argv)
     if len(sys.argv)<=1:
         print("请输入薪资")
         exit(0)
@@ -52,3 +58,4 @@ def getarg():
 
 if __name__=="__main__":
     getarg()
+    printResult()
